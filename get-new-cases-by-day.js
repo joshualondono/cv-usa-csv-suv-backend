e2a8e10e56ddf68c05364920dde6c0950993936d
joshua londono <joshualondono@outlook.com>
2020-07-16 10:33:40 -0400

    need to clean up get-rows

--- get-new-cases-by-day.js
+++ get-new-cases-by-day.js
@@ -1,6 +1,11 @@
+const getNewCasesByDay = function(date, array){
+  for (arr of array){
+    if(date === arr[3]){
+      return arr[5]
+    }
+  }
 
-
-
+}
 
 
 if (typeof getNewCasesByDay === 'undefined') {
